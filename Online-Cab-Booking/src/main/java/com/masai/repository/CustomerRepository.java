b2a23dc1d7727406bec1e893f5afbf82d17e8047
your github user namefaisal1205 <mirzfaisa14@yahoo.in>
2022-08-11 12:55:43 +0530

    CustomerRepository Created

---
+++ Online-Cab-Booking/src/main/java/com/masai/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.masai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.entities.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+	
+
+}
