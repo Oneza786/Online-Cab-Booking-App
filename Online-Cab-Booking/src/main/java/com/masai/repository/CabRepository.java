6d4a4ebc9fd5b850ef2edca5d620c9fe233f57ec
Vishal9sep <vishal.9476300091@gmail.com>
2022-08-11 18:22:42 +0530

    Added Cab Repo

---
+++ Online-Cab-Booking/src/main/java/com/masai/repository/CabRepository.java
@@ -0,0 +1,11 @@
+package com.masai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.entities.Cab;
+
+public interface CabRepository extends JpaRepository<Cab, Integer> {
+	
+	
+
+}
