0e938cfa1d88f246d074bd2ed19e8b3f99499b12
Oneza Tehreem <tehreem09oneza@gmail.com>
2022-08-11 13:26:04 +0530

    Trip Details Repo added

---
+++ Online-Cab-Booking/src/main/java/com/masai/repository/TripDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.masai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.entities.TripDetails;
+
+public interface TripDetailsRepository extends JpaRepository<TripDetails, Integer>{
+
+}
